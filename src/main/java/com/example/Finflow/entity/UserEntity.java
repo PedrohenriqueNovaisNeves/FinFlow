fa108df355777f3e9576fef471041125f380b18b
Pedro Neves <pp5709353@gmail.com>
2026-01-28 19:26:22 -0300

    user entity class created

---
+++ src/main/java/com/example/Finflow/entity/UserEntity.java
@@ -0,0 +1,35 @@
+package com.example.Finflow.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Entity
+@Table(name = "TB_USER")
+@Getter
+@Setter
+public class UserEntity {
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private UUID idUser;
+    @Column(nullable = false)
+    private String nameUser;
+    @Column(nullable = false, unique = true)
+    private int numberAccount;
+    @Column(nullable = false, unique = true)
+    private String cpfUser;
+    @Column(nullable = false)
+    private LocalDateTime creationDate;
+    @Column(nullable = false, unique = true)
+    private String email;
+    @Column(nullable = false, unique = true)
+    private String password;
+    private LocalDateTime dateOfExclusion;
+    
+}
