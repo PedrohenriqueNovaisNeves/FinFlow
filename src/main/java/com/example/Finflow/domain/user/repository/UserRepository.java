68d45931525acbda82cbd1ad3c5e314f891c1ca7
Pedro Neves <pp5709353@gmail.com>
2026-02-09 22:20:00 -0300

    user repository class created

---
+++ src/main/java/com/example/Finflow/domain/user/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package com.example.Finflow.domain.user.repository;
+
+import com.example.Finflow.entity.UserEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.UUID;
+
+public interface UserRepository extends JpaRepository<UUID, UserEntity> {
+}
