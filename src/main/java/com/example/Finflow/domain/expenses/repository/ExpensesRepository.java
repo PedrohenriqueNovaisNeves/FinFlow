1a636f254fd35dbda3d609b7952c23254982751d
Pedro Neves <pp5709353@gmail.com>
2026-02-09 22:18:53 -0300

    add in remote repository expenses repository class

---
+++ src/main/java/com/example/Finflow/domain/expenses/repository/ExpensesRepository.java
@@ -0,0 +1,9 @@
+package com.example.Finflow.domain.expenses.repository;
+
+import com.example.Finflow.entity.ExpensesEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.UUID;
+
+public interface ExpensesRepository extends JpaRepository<UUID, ExpensesEntity> {
+}
